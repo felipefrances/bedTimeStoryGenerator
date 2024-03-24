c5d8076d9acff0418ae86fa98dbd55f18c9717c7
RaissaParente <raissabparente@gmail.com>
2024-03-24 16:09:59 -0300

    first json created

--- Sources/WordBank.swift
+++ Sources/WordBank.swift
@@ -9,10 +9,15 @@ import Foundation
 
 struct WordBank: Codable {
     
-    var characterBank: [String] = []
-    var descriptorBank: [String] = []
-    var actionBank: [String] = []
-    var scenarioBank: [String] = []
+    var characterBank: [String] = [
+        "pirate", "horse", "princess", "fairy", "knight", "wizard", "mermaid", "unicorn", "explorer", "inventor", "baker", "gardener", "firefighter", "teacher", "dragon"
+    ]
+    var descriptorBank: [String] = [
+        "lost", "happy", "hungry", "lonely", "brave", "curious", "kind", "silly", "helpful", "adventurous", "playful", "determined", "clever", "friendly"]
+    var actionBank: [String] = [
+        "fighting", "running", "dancing", "reading", "skipping", "giggling", "exploring", "climbing", "helping", "singing", "whispering", "building", "dreaming", "painting"]
+    var scenarioBank: [String] = [
+        "farm", "castle", "meadow", "island", "forest", "cottage", "beach", "kingdom", "marketplace", "sea", "hills", "cave", "treehouse", "sky", "volcano", "ship"]
     
     //writing the word bank in a json file
     func saveBank() {
@@ -26,7 +31,7 @@ struct WordBank: Codable {
             }
             
         } catch {
-            print("Error")
+            print("error saving")
         }
     }
     
@@ -35,14 +40,23 @@ struct WordBank: Codable {
         do {
             if let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
                 let fileURL = documentDirectory.appendingPathComponent("data.json")
-                let data = try Data(contentsOf: fileURL)
                 
-                let decoder = JSONDecoder()
-                let model = try decoder.decode(WordBank.self, from: data)
-                return model
+                //checks if theres json yet
+                if FileManager.default.fileExists(atPath: fileURL.path) {
+                    //translates json to struct
+                    let data = try Data(contentsOf: fileURL)
+                    let decoder = JSONDecoder()
+                    let model = try decoder.decode(WordBank.self, from: data)
+                    return model
+                } else {
+                    //created json if there isnt any
+                    let defaultWordBank = WordBank()
+                    defaultWordBank.saveBank()
+                    return defaultWordBank
+                }
             }
         } catch {
-            print("error")
+            print("error loading")
         }
         return nil
     }
